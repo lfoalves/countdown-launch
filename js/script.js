054a23a16e016ed84b668f68c67741c755177363
lfoalves <lfoalves.dev@gmail.com>
2022-04-02 16:17:45 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+import Countdown from "./countdown.js";
+
+const tempoParaOWWB = new Countdown("07 April 2022 19:00:00 GMT-0300")
+
+const tempos = document.querySelectorAll("[data-time");
+
+function mostrarTempo() {
+  tempos.forEach((tempo, index) => {
+    tempo.innerHTML = tempoParaOWWB.total[index]
+  
+  });
+}
+
+mostrarTempo();
+setInterval(mostrarTempo, 1000)
+
+
