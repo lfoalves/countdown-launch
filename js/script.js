2b3c9273d48778a6d6a14ea99573bc4f6fee5cee
lfoalves <lfoalves.dev@gmail.com>
2022-04-13 22:45:33 -0400

    atualizando a hora

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from "./countdown.js";
 
-const tempoParaOWWB = new Countdown("07 April 2022 19:00:00 GMT-0300")
+const tempoParaOWWB = new Countdown("07 May 2022 00:00:00 GMT-0300")
 
 const tempos = document.querySelectorAll("[data-time");
 
